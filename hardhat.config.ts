17dbd10894924134be161f383ba2a153743536c9
Kevin @ Juicy.bet <kiwi@juicy.bet>
2021-07-04 01:57:40 +0200

    Make test config work

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,7 +30,6 @@ task('accounts', 'Prints the list of accounts', async (args, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
-  defaultNetwork: 'testnet',
   networks: {
     localhost: {
       url: 'http://127.0.0.1:8545',
