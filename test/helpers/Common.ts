f9c6617831579b2637b1c9cca9f44ca3fbba2584
Kevin @ Juicy.bet <kiwi@juicy.bet>
2021-07-31 15:29:54 +0200

    Update test helpers to be a bit more accurate

--- test/helpers/Common.ts
+++ test/helpers/Common.ts
@@ -2,8 +2,8 @@ import { BigNumber, constants } from 'ethers/lib/ethers'
 import { ethers } from 'hardhat'
 import './NumberExtensions'
 
-export async function fastForward(t: number, dt = 60) {
-  await ethers.provider.send('evm_increaseTime', [t + dt])
+export async function fastForward(t: number) {
+  await ethers.provider.send('evm_increaseTime', [t])
   await ethers.provider.send('evm_mine', [])
 }
 
@@ -19,8 +19,7 @@ let t = Date.now().ms
 export async function snapshot(callback: (t: number) => Promise<any>) {
   t += (1).year
   await ethers.provider.send('evm_setNextBlockTimestamp', [t])
-  await ethers.provider.send('evm_mine', [])
-  await callback(t + 1)
+  await callback(t)
 }
 
 export function hashEndingWith(s: string) {
